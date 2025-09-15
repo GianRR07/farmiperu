1f5e4886ed4d51a2946ff7fee60a11478199fcbe
GianRR07 <gianfrancorobert@gmail.com>
2025-09-15 11:38:20 -0500

    Pagina de quienes somos completa

--- src/Qsomos.js
+++ src/Qsomos.js
@@ -1,12 +1,68 @@
-import React from 'react';
-import './Inicio.css';
-
+import React, { useState } from 'react';
+import './Qsomos.css';  
 function Qsomos() {
+  const [misionVisible, setMisionVisible] = useState(false);
+  const [visionVisible, setVisionVisible] = useState(false);
+
   return (
-    <div className="contenedor-inicio">
+    <div className="contenedor-qsomos">
       <main className="contenido">
-        <h2>aca las webadas de quienes somo , mision , vision , etc</h2>
         
+        <p>
+          Somos una empresa Peruana dedicada al Cuidado de la Salud, Comercialización,
+          Dispensación, Expendio de calidad, de Productos Farmacéuticos, Dispositivos Médicos
+          y Productos Sanitarios.
+        </p>
+
+        <div className="flipboxes">
+          <div
+            className={`flipbox ${misionVisible ? "show" : ""}`}
+            onClick={() => setMisionVisible(!misionVisible)}
+          >
+            <h3>{misionVisible ? "" : "Misión"}</h3>
+            <p>
+              Brindar un servicio de calidad, ofreciendo productos seguros y eficaces para
+              cuidar la salud con excelentes asesoramientos farmacéuticos solucionando los diversos problemas de salud de la comunidad, acorde a las exigencias de las familias peruanas.
+            </p>
+          </div>
+
+          <div
+            className={`flipbox ${visionVisible ? "show" : ""}`}
+            onClick={() => setVisionVisible(!visionVisible)}
+          >
+            <h3>{visionVisible ? "" : "Visión"}</h3>
+            <p>
+              Ser el Establecimiento Farmacéutico “Farmacia” peruano con autenticidad,
+              reconocimiento y aceptación por los ciudadanos en cuidado del bienestar y la salud de todas las familias peruanas.
+            </p>
+          </div>
+        </div>
+
+        <div className="franja-roja">
+          <p>Como profesionales de la salud tenemos más de 10 años en el sector salud cuidando la salud y el bienestar de todas las familias peruanas.</p>
+        </div>
+
+        <div className="nuestros-valores">
+          <h2>Nuestros valores</h2>
+          <div className="valores-container">
+            <div className="valor">
+              <img src={require('./images/valor1.PNG')} alt="valor1" />
+              <p>Personas capacitadas y<br></br> honestas al cumplir sus funciones</p>
+            </div>
+            <div className="valor">
+              <img src={require('./images/valor2.PNG')} alt="valor2" />
+              <p>Dedicados a la atención personalizada<br></br> de nuestros pacientes y sus necesidades</p>
+            </div>
+            <div className="valor">
+              <img src={require('./images/valor3.PNG')} alt="valor3" />
+              <p>Comprometidos con la salud y<br></br> el bienestar de nuestros pacientes</p>
+            </div>
+            <div className="valor">
+              <img src={require('./images/valor4.PNG')} alt="valor4" />
+              <p>Personal con liderazgo <br></br>absoluto</p>
+            </div>
+          </div>
+        </div>
       </main>
     </div>
   );
