10a75fcf7710b28acfb69a94b90fe9e710836321
Joseph R <josephr110600@gmail.com>
2025-09-23 00:33:59 -0500

    Resolví conflictos y mantengo mi versión

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,9 @@ import Productos from "./pages/Productos";
 import Login from "./pages/Login";
 import CarritoCompras from "./pages/CarritoCompras";
 import QuienesSomos from './pages/QuienesSomos';
-<<<<<<< HEAD
 import Contacto from './pages/Contacto';
-import LoginCliente from './pages/LoginCliente'; 
-=======
-import Contacto from './pages/Contacto'; 
+import LoginCliente from './pages/LoginCliente';
 import Administrador from './pages/Administrador';
->>>>>>> a5a0360ad4975dbedacf8b2b79766e0f66f89557
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -31,14 +27,12 @@ export default function App() {
             <Route path="/productos" element={<Productos />} />
             <Route path="/login" element={<Login />} />
             <Route path="/carrito" element={<CarritoCompras />} />
-<<<<<<< HEAD
+
             <Route path="/contacto" element={<Contacto />} />
             <Route path="/cliente" element={<LoginCliente />} />
-             <Route path="/admin" element={<Administrador />} />
-=======
+            <Route path="/admin" element={<Administrador />} />
             <Route path="/contacto" element={<Contacto />} /> 
             <Route path="/admin" element={<Administrador />} />
->>>>>>> a5a0360ad4975dbedacf8b2b79766e0f66f89557
           </Routes>
         </main>
         <Footer />
@@ -48,5 +42,4 @@ export default function App() {
 }
 
 
-import Administrador from './pages/Administrador';
 
